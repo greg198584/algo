c340da2e148eb2f8c32d22ca50358213717d620c
Gregory LAFITTE <glafitte@e1r2p22.42.fr>
2015-01-19 13:40:53 +0100

    modif strlen - et .h

--- libft/src/ft_strlen.c
+++ libft/src/ft_strlen.c
@@ -6,16 +6,38 @@
 /*   By: glafitte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 17:01:54 by glafitte          #+#    #+#             */
-/*   Updated: 2014/11/05 09:34:16 by glafitte         ###   ########.fr       */
+/*   Updated: 2015/01/19 13:39:14 by glafitte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdint.h>
+#include <string.h>
+
 int	ft_strlen(const char *str)
 {
-	int	i;
+	const char				*p;
+	const unsigned long		*lp;
+	int						x;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+	p = str;
+	while ((uintptr_t)p & (sizeof(long) - 1))
+		if (*p++ == '\0')
+			return ((p - 1) - str);
+	lp = (const unsigned long *)p;
+	while (1)
+		if ((*lp++ - 0x0101010101010101) & 0x8080808080808080)
+		{
+			x = -1;
+			p = (const char *)(lp - 1);
+			while (++x < 8)
+			{
+				while (1)
+				{
+					if (!p[x])
+						return (p - str + x);
+					break ;
+				}
+			}
+		}
+	return (0);
 }
